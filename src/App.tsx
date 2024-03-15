fc05878a8ee9a8ad7a604054e18331dd5ffcdb3f
Jeremy <jeremy@jeremylanger.com>
2024-03-15 11:34:06 -0600

    Show one word at a time

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,21 @@ const sentences = [
 ];
 
 function App() {
+  const [partialSentence, setPartialSentence] = useState("");
   const [sentenceIndex, setSentenceIndex] = useState(0);
 
   const handleKeydown = useCallback((event: KeyboardEvent) => {
-    if (event.key === "ArrowRight" || event.key === " ") setSentenceIndex(curr => Math.min(sentences.length - 1, curr + 1));
-    if (event.key === "ArrowLeft") setSentenceIndex(curr => Math.max(0, curr - 1));
+    if (event.key === "ArrowRight" || event.key === " ") {
+      // setPartialSentence("");
+      setSentenceIndex(curr => Math.min(sentences.length - 1, curr + 1));
+    }
+    if (event.key === "ArrowLeft") {
+      // setPartialSentence("");
+      setSentenceIndex(curr => Math.max(0, curr - 1));
+    }
   }, []);
+
+  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeydown, false);
@@ -21,9 +30,31 @@ function App() {
     return () => document.removeEventListener("keydown", handleKeydown, false);
   }, [handleKeydown]);
 
+  useEffect(() => {
+    let cancel = false;
+    const animateSentence = async () => {
+      const split = sentences[sentenceIndex].trim().split(" ");
+      for (const word of split) {
+        if (cancel) return;
+        setPartialSentence(curr => (curr + " " + word).trim());
+        await sleep(250);
+      }
+    };
+
+    animateSentence();
+
+    return () => {
+      setPartialSentence("");
+      cancel = true;
+    }
+  }, [sentenceIndex]);
+
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-[url('/dune-wallpaper.jpg')] text-center text-white">
-      <div className="font-dm-serif text-5xl max-w-[800px] m-auto mt-[360px] leading-tight" dangerouslySetInnerHTML={{__html: sentences[sentenceIndex]}} />
+    <div className="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-[url('/dune-wallpaper.jpg')] text-white font-dm-serif text-5xl leading-tight">
+      <div className="max-w-[800px] m-auto mt-[360px]">
+        <span className="inline-block" dangerouslySetInnerHTML={{__html: partialSentence}} />
+        <span className="inline-block"></span>
+      </div>
     </div>
   );
 }
