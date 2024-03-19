e11d130c6f17591fb9569f7faa75b098826536e4
Jeremy <jeremy@jeremylanger.com>
2024-03-19 11:48:34 -0600

    Show sentence index as the range is changing

--- src/App.tsx
+++ src/App.tsx
@@ -102,7 +102,10 @@ function App() {
       </div>
       
       <div className="group fixed bottom-0 left-0 right-0 h-2 hover:h-4 z-10 transition-all">
-        <input type="range" className="block appearance-none w-full h-full bg-[rgba(255,255,255,0.25)] rounded-md outline-none [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-2 [&::-webkit-slider-thumb]:w-2 group-hover:[&::-webkit-slider-thumb]:h-4 group-hover:[&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-[rgba(255,255,255,0.5)] cursor-pointer" max={sentences.length} value={sentenceIndex} onChange={handleRangeChange} />
+        <input type="range" className="peer block appearance-none w-full h-full bg-[rgba(255,255,255,0.25)] rounded-md outline-none [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-2 [&::-webkit-slider-thumb]:w-2 group-hover:[&::-webkit-slider-thumb]:h-4 group-hover:[&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-[rgba(255,255,255,0.5)] cursor-pointer" max={sentences.length} value={sentenceIndex} onChange={handleRangeChange} />
+        <div className="hidden peer-active:block text-base absolute bottom-4">
+          <span className="inline-block" style={{ marginLeft: `${(sentenceIndex/sentences.length)*100}vw` }}>{sentenceIndex}</span>
+        </div>
       </div>
     </div>
   );
