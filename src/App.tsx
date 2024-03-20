f43492530b1297376166082c70550491cdf3c2ae
Jeremy <jeremy@jeremylanger.com>
2024-03-19 21:49:08 -0600

    Keep sentence index in the url

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import { NextWord } from "./NextWord";
 import { openDuneEpub } from "./epubReader";
 
 const chapters = await openDuneEpub();
+console.log({ chapters });
 const sentences = chapters?.flat() ?? [];
 
 function App() {
-  const savedSentenceIndex = +(localStorage.getItem("sentenceIndex") || 0);
-  const [sentenceIndex, setSentenceIndex] = useState(savedSentenceIndex);
+  const [sentenceIndex, setSentenceIndex] = useState(() => {
+    const params = new URLSearchParams(window.location.search);
+    const p = parseInt(params.get('p') || '0');
+    // Keep it between the first and last page
+    return Math.min(sentences.length - 1, Math.max(0, p));
+  });
 
   const goBack = () => setSentenceIndex(curr => Math.max(0, curr - 1));
   const goForward = () => setSentenceIndex(curr => Math.min(sentences.length - 1, curr + 1));
@@ -30,7 +35,10 @@ function App() {
   }, [handleKeydown]);
 
   useEffect(() => {
-    localStorage.setItem("sentenceIndex", sentenceIndex.toString());
+    // Save our spot in the url
+    const params = new URLSearchParams();
+    params.append('p', sentenceIndex.toString());
+    window.history.pushState({}, '', `?${params.toString()}`);
   }, [sentenceIndex]);
 
   const calculateDelay = (word: string) => {
@@ -56,7 +64,7 @@ function App() {
   const startOfItalicPhrase = (word: string) => word.startsWith("<em>");
   const endOfItalicPhrase = (word: string) => (word.endsWith("</em>") || word.endsWith("</em>,") || word.endsWith("</em>."));
 
-  const getWordsFromElement = (el: Element) => {
+  const getWordsFromElement = (el: Element): string => {
     if (el.nodeName === "p") return formatParagraph(el.innerHTML);
     if (el.nodeName === "blockquote") {
       const paragraphs = Array.from(el.children).map((child: Element) => formatParagraph(child.innerHTML));
@@ -102,8 +110,8 @@ function App() {
       </div>
       
       <div className="group fixed bottom-0 left-0 right-0 h-2 hover:h-4 z-10 transition-all">
-        <input type="range" className="peer block appearance-none w-full h-full bg-[rgba(255,255,255,0.25)] rounded-md outline-none [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-2 [&::-webkit-slider-thumb]:w-2 group-hover:[&::-webkit-slider-thumb]:h-4 group-hover:[&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-[rgba(255,255,255,0.5)] cursor-pointer" max={sentences.length} value={sentenceIndex} onChange={handleRangeChange} />
-        <div className="hidden peer-active:block text-base absolute bottom-4">
+        <input type="range" className="peer block appearance-none w-full h-full bg-[rgba(255,255,255,0.25)] rounded-md outline-none [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-2 [&::-webkit-slider-thumb]:w-2 group-hover:[&::-webkit-slider-thumb]:h-4 group-hover:[&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-[rgba(255,255,255,0.5)] cursor-pointer" max={sentences.length - 1} value={sentenceIndex} onChange={handleRangeChange} />
+        <div className="hidden peer-hover:block peer-active:block text-base absolute bottom-4">
           <span className="inline-block" style={{ marginLeft: `${(sentenceIndex/sentences.length)*100}vw` }}>{sentenceIndex}</span>
         </div>
       </div>
