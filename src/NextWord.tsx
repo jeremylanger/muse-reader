a2494a1db79ec57facf1783e40508eeeaf91f2e7
Jeremy <jeremy@jeremylanger.com>
2024-03-30 20:18:44 -0600

    Look up definition of word on click

--- src/NextWord.tsx
+++ src/NextWord.tsx
@@ -8,6 +8,19 @@ interface Props {
 export const NextWord = ({ delay, word }: Props) => {
 	const [fadeIn, setFadeIn] = useState(false);
 
+	const handleClick = () => {
+		const cleanedWord = word.replace(/<[^>]*>/g, "").replace(/[^a-zA-Z-]+/g, "");
+		fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${cleanedWord}`, {
+			method: "GET",
+			headers: {
+				"Accept": "application/json",
+			}
+		})
+			.then(response => response.json())
+			.then(data => console.log(data))
+			.catch(error => console.error(error));
+	};
+
 	useEffect(() => {
 		setTimeout(() => {
 			setFadeIn(true);
@@ -15,7 +28,7 @@ export const NextWord = ({ delay, word }: Props) => {
 	}, [delay]);
 
 	return <>
-		<span className={`${fadeIn ? "opacity-100" : "opacity-0"} duration-500 mr-2 sm:mr-3 transition-opacity inline-block`} dangerouslySetInnerHTML={{__html: word}} />
+		<span onClick={handleClick} className={`${fadeIn ? "opacity-100" : "opacity-0"} duration-500 mr-2 sm:mr-3 transition-opacity inline-block`} dangerouslySetInnerHTML={{__html: word}} />
 		{word.endsWith("<br>") && <br />}
 	</>;
 }
